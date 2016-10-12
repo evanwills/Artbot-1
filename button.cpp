1209c26dc4190684916bed6d43e1cf16de4031a3
Evan Wills <evan.wills@acu.edu.au>
2016-10-13 07:39:15 +1100

    added working code and lots of comments.

--- button.cpp
+++ button.cpp
@@ -1,22 +1,79 @@
-#include button.h
+#include TimedButton.h
 
 
-button::button(byte pin) {
+TimedButton::TimedButton(byte pin) {
 	btnPin = pin;
 	pinMode(btnPin, INPUT);
 }
 
-void button::readButtion() {
+
+bool TimedButton::readButton() {
 	if (digitalRead(incrementBtn) == HIGH) {
-		if (pressedForLoops < 65,535 ) {
-			pressedForLoops += 1;
+		if (start == 0 ) {
+			start = millis();
 		}
+		// update how long the button has been pressed for
+		pressDuration = millis() - start;
+		inUse = true;
 	} else {
-		pressedForLoops = 0;
+		// button is no longer being pressed.
+		// we keep the press duration for one call so
+		// that buttonPressedFor() can be used effectively
+		if( inUse == false ) {
+			// button was being pressed two calls ago
+			// reset for next press
+			start = 0;
+			pressDuration = 0;
+		}
+		// button is no longer being pressed
+		inUse = false;
 	}
+	return inUse;
 }
 
-unsigned int button::buttonPressedFor() {
-	readButtion();
-	return pressedForLoops;
+/**
+ * How many seconds has the button been pressed for
+ * If it's currently being pressed then it returns -1 to let caller
+ * know that it can't give a final answer yet.
+ * If it's not being pressed, then it returns zero.
+ * If the button has just been released, it returns the number of
+ * milliseconds the button was pressed for.
+ *
+ * NOTE: if you are using both readButton() & buttonPressedFor(),
+ *		 you the doRead parameter needs to be false otherwise, you'll
+ *		 never get a value greater than zero
+ *
+ *		 e.g. (BEST)
+ *			TimedButton btn = new TimedButtion(1);
+ *			int val;
+ *			val = buttonPressedFor();
+ *
+ *		 e.g. (GOOD)
+ *			TimedButton btn = new TimedButtion(1);
+ *			int val;
+ *			btn->readButton();
+ *			val = buttonPressedFor(false); // val: 0 or -1
+ *
+ *		 e.g. (BAD)
+ *			TimedButton btn = new TimedButtion(1);
+ *			int val;
+ *			btn->readButton();
+ *			val = buttonPressedFor(); // val: 0 or -1
+ *			// val will always be either -1 or 0 because pressDuration
+ *			// is set when readButton() is called for a second time
+ *			// with the before buttonPressedFor() can use the
+ *			// pressDuration value
+ */
+int TimedButton::buttonPressedFor( bool doRead ) {
+	if( doRead == true ) {
+		readButtion();
+	}
+
+	if( inUse == true ) {
+		// let the caller know that the button is being pressed
+		// but that we don't know for how long yet.
+		return -1;
+	} else {
+		return (int) pressDuration;
+	}
 }
--- button.h
+++ button.h
@@ -1,11 +1,43 @@
-public class button {
+/**
+ * TimedButton is useful for when you want to do time
+ * based things with a button
+ * e.g. a short click of the button (say, less than 1 second) is used
+ *		for modifying settings but a long click is used for resetting
+ *		to defaults
+ */
+public class TimedButton {
 
 	public:
-		void button(byte pin);
-		void readButton();
-		unsigned int buttonPresedFor();
+		ArtbotButton(byte pin);
+		/**
+		 * If button is pressed, starts counting the number of
+		 * milliseconds since start and returns true. If not,
+		 * resets the timer and returns false.
+		 */
+		bool readButton();
+
+		/**
+		 * reads the button's state then returns the number of
+		 * milliseconds the buttons was pressed for.
+		 */
+		int buttonPresedFor( bool doRead = true );
 
 	private:
+		/*
+		 * btnPin the Arduino Mega pin number used by the button
+		 */
 		byte btnPin = 0;
-		byte pressedForLoops = 0;
+
+		/**
+		 * inUse whether or not the button is being pressed.
+		 */
+		bool inUse = false;
+
+		int pressDuration = 0;
+
+		/**
+		 * start the time in milliseconds (from when the Arduino was
+		 * turned on) the button was pressed.
+		 */
+		unsigned long start = 0;
 }
